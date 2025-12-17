6a3b3f7e3bf26257e392db675bd0bc8f4f6e423b
unknown <harshit.a@moondive.co>
2025-12-17 13:22:53 +0530

    convert frontend from submodule to normal folder

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function App() {
+  const [msg, setMsg] = useState("");
+
+  const callBackend = async () => {
+    const res = await fetch("http://10.0.2.54:3000/task");
+
+    const data = await res.json();
+    setMsg(data.message);
+  };
+
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <button onClick={callBackend}>Run Task</button>
+      <h2>{msg}</h2>
+    </div>
+  );
+}
+
+export default App;
