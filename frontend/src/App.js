817281299df6c5158b80991fd32659e0db363c03
unknown <harshit.a@moondive.co>
2025-12-18 15:40:24 +0530

    updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [msg, setMsg] = useState("");
 
   const callBackend = async () => {
-    const res = await fetch("/task");
+    const res = await fetch("http://10.0.2.116:3000/task");
 
 
     const data = await res.json();
